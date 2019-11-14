916c8bf47b353cb3eb6e4485882f4cfe96a5914d
alexbarksdale <ale.barksdale@gmail.com>
2019-11-14 13:47:44 -0800

    [Add] clientWidth

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 function makeSlideShow(slidesID) {
-    const slides = document.getElementById('slides');
+    const slides = document.getElementById(slidesID);
     const slidesInner = slides.querySelector('.slides-inner'); //?  Why didn't we use getElementByClassName
     const images = slidesInner.querySelectorAll('img'); //?  Why didn't we use getElementByTag
+
+    const slidesWidth = slides.clientWidth;
     let index = 0; // Represents the slide
 
     setInterval(() => {
@@ -9,8 +11,9 @@ function makeSlideShow(slidesID) {
         if (index === images.length) {
             index = 0; // Resets the position
         }
-        slidesInner.style = `transform: translate3d(${index * -1200}px, 0, 0)`;
+        slidesInner.style = `transform: translate3d(${index * -slidesWidth}px, 0, 0)`;
     }, 2000);
 }
 
 makeSlideShow('slides');
+makeSlideShow('slides-2');
