3df9516a8031c12398c0d69fe228fab49ce79550
alexbarksdale <ale.barksdale@gmail.com>
2019-11-14 18:09:46 -0800

    [Add] Wrapped function inside an IIFE

---
+++ slide-show.js
@@ -0,0 +1,28 @@
+// IIFE
+(function() {
+    function makeSlideShow(slides) {
+        const slidesInner = slides.querySelector('.slides-inner'); //?  Why didn't we use getElementByClassName
+        const images = slidesInner.querySelectorAll('img'); //?  Why didn't we use getElementByTag
+
+        const delay = parseInt(slides.dataset.delay);
+        const transition = parseInt(slides.dataset.transition);
+
+        const slidesWidth = slides.clientWidth;
+        let index = 0; // Represents the slide
+
+        setInterval(() => {
+            index++;
+            if (index === images.length) {
+                index = 0; // Resets the position
+            }
+            slidesInner.style = `transform: translate3d(${index * -slidesWidth}px, 0, 0)`;
+            slidesInner.style.transition = `${transition}ms`;
+        }, delay);
+    }
+
+    const slideShows = document.querySelectorAll('.ms-slide-show');
+
+    for (let i = 0; i < slideShows.length; i++) {
+        makeSlideShow(slideShows[i]);
+    }
+})();
